893b3001ee85408624acbfd4fe919d6983f8e754
wyj <wyj@DESKTOP-96H1KMN>
2019-10-27 21:10:21 +0800

    Updates

---
+++ precache-manifest.5e6f05641ebb2747b5e1f2fd6a9fd110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03500bb5ef8298d33a62f6b80b3cd54e",
+    "url": "/toMyFavoriteGirl/index.html"
+  },
+  {
+    "revision": "525cc145cbd3d524f178",
+    "url": "/toMyFavoriteGirl/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "190429bc1a14734de63c",
+    "url": "/toMyFavoriteGirl/static/js/2.085ff987.chunk.js"
+  },
+  {
+    "revision": "525cc145cbd3d524f178",
+    "url": "/toMyFavoriteGirl/static/js/main.03d04c17.chunk.js"
+  },
+  {
+    "revision": "bdbc3842a99cf20c3aaf",
+    "url": "/toMyFavoriteGirl/static/js/runtime-main.60672963.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/toMyFavoriteGirl/static/media/logo.25bf045c.svg"
+  }
+]);
